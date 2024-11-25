c3f18e6c68cabde88e7404e54ff76904d306be56
Anna Taylor <annartaylor@arizona.edu>
2024-11-25 16:17:48 -0700

    Adding Shomate Fit Equation Code

---
+++ Shomate_Fit/Carbon_Shomate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 15:47:30 2024
+
+Fitting the Carbon Enthaply Values to a Shomate Equation 
+
+@author: Anna Ruth Taylor
+"""
+
+import numpy as np
+import matplotlib.pyplot as py
+from scipy.optimize import curve_fit
+
+# Read Janaff Table
+carbon_janaff = np.genfromtxt("JANAF_C.txt")
+temperature = carbon_janaff[:,0]/1000 #K
+H = carbon_janaff[:,4] #kJ/mol
+
+#Define Shomate Equation
+def shomate(t, A, B, C, D):
+    return A*t + (B*t**2)/2 + (C*t**3)/3 + (D*t**4)/4 #- E/t + F - H
+
+def shomate1(t, A, B, C, D, E, F, H):
+    return A*t + (B*t**2)/2 + (C*t**3)/3 + (D*t**4)/4 - E/t + F - H
+
+#Using scipy curve_fit to fit carbon data to shomate equation
+popt, pcov = curve_fit(shomate, temperature, H)
+
+#Plotting Fit 
+fig = py.figure(figsize=(8, 6))
+py.plot(temperature*1000, H, 'o', linewidth = 3, color = 'k' ,label = "JANAF Data")
+py.plot(temperature*1000, shomate(temperature, *popt), color = 'deeppink', linewidth = 3, label = "Curve Fit")
+py.xlabel("Temperature (K)")
+py.ylabel("Standard Enthalpy (kJ/mol)")
+py.legend()
+py.show()
+
+#Printing Shomate Coefficients
+print("Shomate Coefficients: A = ", popt[0], ", B = ", popt[1], ", C = ", popt[2], ", D = ", popt[3], ", E = ", 0.0,  ", F = ", 0.0,  ", H = ", 0.0 )
