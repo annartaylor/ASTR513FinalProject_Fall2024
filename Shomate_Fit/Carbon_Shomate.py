fe3599abcf994f11aa5f7cf8a3c2de46dc4cb876
Anna Taylor <annartaylor@arizona.edu>
2024-11-25 16:25:54 -0700

    Shomate Code

--- Shomate_Fit/Carbon_Shomate.py
+++ Shomate_Fit/Carbon_Shomate.py
@@ -5,7 +5,7 @@ Created on Mon Nov 25 15:47:30 2024
 
 Fitting the Carbon Enthaply Values to a Shomate Equation 
 
-@author: Anna Ruth Taylor
+@author: annartaylor
 """
 
 import numpy as np
@@ -13,23 +13,20 @@ import matplotlib.pyplot as py
 from scipy.optimize import curve_fit
 
 # Read Janaff Table
-carbon_janaff = np.genfromtxt("JANAF_C.txt")
+carbon_janaff = np.genfromtxt("/Users/annartaylor/Library/CloudStorage/OneDrive-UniversityofArizona/Desktop/Fall 2024 Classes/ASTR513/JANAF_C.txt")
 temperature = carbon_janaff[:,0]/1000 #K
-H = carbon_janaff[:,4] #kJ/mol
+enthalpy = carbon_janaff[:,4] #kJ/mol
 
 #Define Shomate Equation
 def shomate(t, A, B, C, D):
     return A*t + (B*t**2)/2 + (C*t**3)/3 + (D*t**4)/4 #- E/t + F - H
 
-def shomate1(t, A, B, C, D, E, F, H):
-    return A*t + (B*t**2)/2 + (C*t**3)/3 + (D*t**4)/4 - E/t + F - H
-
 #Using scipy curve_fit to fit carbon data to shomate equation
-popt, pcov = curve_fit(shomate, temperature, H)
+popt, pcov = curve_fit(shomate, temperature, enthalpy)
 
 #Plotting Fit 
 fig = py.figure(figsize=(8, 6))
-py.plot(temperature*1000, H, 'o', linewidth = 3, color = 'k' ,label = "JANAF Data")
+py.plot(temperature*1000, enthalpy, 'o', linewidth = 3, color = 'k' ,label = "JANAF Data")
 py.plot(temperature*1000, shomate(temperature, *popt), color = 'deeppink', linewidth = 3, label = "Curve Fit")
 py.xlabel("Temperature (K)")
 py.ylabel("Standard Enthalpy (kJ/mol)")
