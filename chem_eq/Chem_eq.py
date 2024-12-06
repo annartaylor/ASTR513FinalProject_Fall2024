27b62bfe4d89aa216f2d7c2833a65b95e9ab4c74
Jingyu Wang <156482353+Jingyu37@users.noreply.github.com>
2024-12-06 09:58:20 +0800

    Update Chem_eq.py clean up

--- chem_eq/Chem_eq.py
+++ chem_eq/Chem_eq.py
@@ -30,17 +30,6 @@ reaction_list = np.genfromtxt("Reactions.dat", dtype="int")
 # loop through all reactions, if all reactants exist, append both reactants and products to a new list
 # major species: CO2, CO, SO2, N2
 species = np.array([10,11,18,13]) # species number, not index
-species_append = species
-
-for i in range(0, len(reaction_list)):
-    if reaction_list[i][2] == 0:
-        if (reaction_list[i][0] in species_append) and (reaction_list[i][1] in species_append):
-            species_append = np.append(species_append,reaction_list[i][-2:])
-    else:
-        if (reaction_list[i][0] in species_append) and (reaction_list[i][1] in species_append) and (reaction_list[i][2] in species_append):
-            species_append = np.append(species_append,reaction_list[i][-2:])
-#print(species)
-species_all = np.array(list(set(species_append[np.where(species_append>0)])))
 species_all = np.array([10,11,18,13])
 print(species_all)
 
@@ -136,118 +125,8 @@ for i in range(0,len(species_all)):
     delta_f_S_m[:,i] = compute_delta_f_S_m(T, shomate, shomate_ref, mu[i])
 print("delta_f_H_m",delta_f_H_m[0,:])
 print("delta_f_S_m",delta_f_S_m[0,:])
-#delta_f_G_m = compute_delta_f_G_m(T, P, mr, delta_f_H_m, delta_f_S_m)
-
-# minimize delta_f_G to get mixing ratios of all speceis
-# sum_delta_f_G = sum(mr_i*delta_f_G_m)
-# conservation of element
-mr_ini = np.array([1.e-6,1.e-6,1.e-5,1.e-5,5.e-6,0.965,3.e-5,0.034,3.e-9,1.5e-4,5.e-6])
 
 element = ['C', 'H', 'O', 'N', 'S']
-
-
-def sum_mr_element(species, mr, element):
-    # compute the sum of number demsity at each altitude for each element: CHONS
-    # first compute the number density at each altitude,
-    # then compute the number density of each species at each altitude
-    sum = np.zeros((len(element),len(height)))
-    MM = np.zeros(len(height))
-    for i in range(0,len(element)):
-        for j in range(0,len(height)):
-            MM[j] = P[j]/(k_B*T[j])
-            for k in range(0,len(species)):
-                # number density n
-                n = mr[j,k]*MM[j]
-                sum[i,j] +=  n*species_data['species'][species[k]-1]['composition'].get(element[i])
-    return sum
-
-# initialize the mixing ratios at equilibrium of all speceis using the initial condition
-mr_eq_ini = np.zeros((len(height),len(species_all)))
-
-for i in range(0, len(species_all)):
-    if species_all[i] in species:
-        mr_eq_ini[:,i] = np.ones_like(height)*mr_ini[np.where(species == species_all[i])[0][0]]
-mr_eq_ini += 1.0e-20 #add on 1.0e-20 to avoid log(0) in the derivative
-#print(mr_eq_ini[0,:])
-
-#print("sum of the number density of each element:", sum_mr_element(species_all, mr_eq_ini, element))
-#print("sum of the number density of each element -1e-14:", sum_mr_element(species_all, mr_eq_ini-1.0e-14, element))
-
-#print("composition of a species",list(species_data['species'][species_all[0]]['composition'].values()))
-
-#print("sum of mr at each altitude:", np.sum(mr_eq_ini, axis = 1))
-
-# minimize sum(delta_f_G_m) using gradient descent
-def gradient_descent(P, T, mr_eq_ini, delta_f_H_m, delta_f_S_m):
-    # inputs:
-    #        P, T
-    #        delta_f_H_m and delta_f_S_m at each altitude
-    # initialization of variables, step, threshold for convergence
-    n_iter = 200
-    stepsize = 1.e-15
-    threshold = 1.e-6
-    mr = mr_eq_ini
-    pi_j = np.zeros((len(element),len(height))) # penalty weight for element conservation
-    gama_1 = 1.0e-5 # learning rate for conservation of element
-
-    mr_old = np.zeros_like(mr) # current mr
-    grad = np.zeros((len(height),len(species_all))) # sum of gradient
-    grad_old = np.zeros((len(height),len(species_all)))
-    grad_g = np.zeros((len(height), len(species_all))) # gradient from constraints
-
-    sum_element_ini = sum_mr_element(species_all, mr_eq_ini, element) # sum of element initially
-
-    sum_stoichiometry=np.zeros(len(element))
-    
-    for j in range(len(species_all)):
-        sum_stoichiometry +=  list(species_data['species'][species_all[j]-1]['composition'].values())
-    #print("sum_stoichiometry:", sum_stoichiometry)
-
-    # iteration
-    for i in range(0, n_iter):
-        grad_f = np.zeros((len(height),len(species_all)))
-
-        # compute gradient and stepping
-        for k in range(len(height)):
-            # assign current mr and gradient to mr_old and gradient_old
-            mr_old[k,:] = mr[k,:]
-            grad_old[k,:] = grad[k,:]
-            # update mr and grad
-            grad_f[k,:] = delta_f_H_m[k,:]*1.0E3-T[k]*delta_f_S_m[k,:]+R_U*T[k]*np.log(mr[k,:]*P[k]/P_not)+R_U*T[k]
-            grad_g[k,:] = np.dot(sum_stoichiometry,pi_j[:,k])
-            grad[k,:] = grad_f[k,:] + grad_g[k,:]
-            print("gradient_f:", grad_f[k,:])
-            # update mr 
-            #print("mr_old:", mr_old[k,:])
-            mr[k,:] = mr_old[k,:] - stepsize*grad[k,:]
-            # new mr should be positive
-            mr = np.maximum(1.0e-30, mr)
-            #print("mr:", mr[k,:])
-
-        # update learning rates for constraints
-        pi_j += gama_1*((sum_mr_element(species_all, mr, element)-sum_element_ini)/sum_element_ini)
-        #print("pi_j:",pi_j)
-        # compare with the threshold for the gradient
-        #print("gradient",grad[0,:])
-        #print("mr old:", mr_old[0,:])
-        # re-scale mr to ensure the sum of mr at each altitude is one
-        #mr[k,:] = mr[k,:]/np.sum(mr[k,:])
-        #print("norm of gradient:", np.linalg.norm(grad))
-        if np.linalg.norm(grad) < threshold:
-            break
-    print("iteration:", i)
-
-    # check conservation of element/mass balance
-    sum_mr_element_eq = sum_mr_element(species_all, mr, element)
-    if ((sum_mr_element_eq-sum_element_ini)/sum_mr_element_eq > threshold).any():
-        print("ERROR:Conservation of element not satisfied!")
-    else:
-        print("Conservation of element satisfied!")
-    return mr
-
-# use gradient descent to find the chemical equilibrium
-#mr_eq = gradient_descent(P, T, mr_eq_ini, delta_f_H_m, delta_f_S_m)
-#print("mr eq:", mr_eq[0,:])
 
 # minimize sum(delta_f_G_m) using grid search
 mr_eq = np.zeros((len(height),len(species))) # output mr for all species
